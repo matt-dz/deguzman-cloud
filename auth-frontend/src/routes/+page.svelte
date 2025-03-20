b19b5a7aac6660781d9ecd223c834e03f5e9c922
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2025-03-20 14:54:23 -0400

    fix: remove border on mobile view

--- auth-frontend/src/routes/+page.svelte
+++ auth-frontend/src/routes/+page.svelte
@@ -9,7 +9,7 @@
 	<div class="flex w-full flex-col">
 		<div class="grid h-full grid-cols-1 md:grid-cols-2">
 			<div class="hidden md:visible md:flex md:items-center"></div>
-			<div class="flex w-full items-center border-l-2 border-solid border-gray-700">
+			<div class="flex w-full items-center md:border-l-2 md:border-solid md:border-gray-700">
 				<Login redirectUrl={data.redirectUrl || undefined} />
 			</div>
 		</div>
