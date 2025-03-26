789bfc2fa8c7ef62ef2bdddc0093b6519a1788b5
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2025-03-25 22:33:04 -0400

    feat: add seo for auth frontend

--- auth-frontend/src/routes/+layout.svelte
+++ auth-frontend/src/routes/+layout.svelte
@@ -3,4 +3,21 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<!-- SEO Meta Tags -->
+	<meta name="description" content="deguzman.cloud log in page." />
+	<meta name="author" content="Matthew DeGuzman" />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:title" content="deguzman.cloud" />
+	<meta property="og:description" content="deguzman.cloud log in page." />
+	<meta property="og:image" content="https://deguzman.cloud/assets/logos/og-image.png" />
+	<meta property="og:url" content="https://auth.deguzman.cloud" />
+	<meta property="og:type" content="website" />
+
+	<!-- Twitter Card -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@matthew_d13" />
+</svelte:head>
+
 {@render children()}
--- auth-frontend/src/routes/signup/+page.svelte
+++ auth-frontend/src/routes/signup/+page.svelte
@@ -5,6 +5,10 @@
 	let { data }: PageProps = $props();
 </script>
 
+<svelte:head>
+	<title>deguzman | sign up</title>
+</svelte:head>
+
 <div class="flex h-dvh items-center">
 	<div class="flex w-full flex-col items-center">
 		<div class="flex w-full max-w-[525px] items-center">
